6c6dcf0a0715058b7f7a9a57bf09178eca728c46
Luis Camacho <luiscamacho@gmail.com>
2015-12-08 21:32:10 -0500

    Checkpoint 22: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -30,6 +30,32 @@ class AddressBook
    csv.each do |row|
      row_hash = row.to_hash
      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
    end
-   end
+   # Search AddressBook for a specific entry by name
+
+    def binary_search(name)
+      lower = 0
+      upper = @entries.length - 1
+
+# #2
+    while lower <= upper
+# #3
+    mid = (lower + upper) / 2
+    mid_name = @entries[mid].name
+
+# #4
+    if name == mid_name
+      return @entries[mid]
+    elsif name < mid_name
+      upper = mid - 1
+    elsif name > mid_name
+      lower = mid + 1
+    end
+  end
+
+# #5
+  return nil
+
+    end
 end
