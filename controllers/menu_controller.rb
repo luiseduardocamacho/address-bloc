ce04007db16be0022531ef6c73893cb6d1f96739
Luis Camacho <luiscamacho@gmail.com>
2015-12-10 21:06:28 -0500

    assignment 23 Added Destroy all functionality

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - Kill All"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -37,6 +38,11 @@ class MenuController
       read_csv
       main_menu
     when 5
+      system "clear"
+      destroy_all
+      main_menu
+
+    when 6
       puts "Good-bye!"
 # #8
       exit(0)
@@ -200,6 +206,10 @@ def view_all_entries
          puts entry.to_s
          search_submenu(entry)
        end
+     end
+
+     def destroy_all
+       @address_book.entries.clear
      end
 
 end
